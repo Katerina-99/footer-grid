de7f939d975774f07f366a4288e3dbbafaf624e1
Katerina-99 <57578765+Katerina-99@users.noreply.github.com>
2021-05-13 12:14:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('.button').click(function () {
+    // console.log('click');
+    var inputValue = $('.footer-input').val();
+    alert(inputValue + ' подписан на рассылку');
+  });
+})
